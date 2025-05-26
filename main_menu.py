1abae50a7dd348a99cd226f5673dae448ef7fbc5
Polina <polina.dubovikova15@yandex.ru>
2025-05-26 08:18:16 -0300

    Навели красоту :)

--- main_menu.py
+++ main_menu.py
@@ -8,6 +8,7 @@ def main_menu():
         confirm = input("Все верно? Чтобы подтвердить заказ, введите 'да' чтобы отклонить заказ, введите 'нет': ")
         if confirm == "да" or confirm == "ДА" or confirm == "Да" or confirm == "дА":
             save_order(order)
+            print("🐱Спасибо за заказ!🐱")
         else:
             continue
 
@@ -45,33 +46,33 @@ def menu(choices, title="Меню", promt="Введите номер: "):
                 answer = choices[int(choice) - 1]
                 break
         else:
-            print("Введите число от 1 до", len(choices), "!")
+            print("🐱Введите число от 1 до", len(choices), "!")
             answer = ""
     return answer
 
 
 def get_order():
     order = {}
-    order["name"] = input("Напишите ваше имя: ")
+    order["name"] = input("🐱Напишите ваше имя: ")
     products = read_menu("products.txt")
     flavors = read_menu("flavors.txt")
     toppings = read_menu("toppings.txt")
-    order["product"] = menu(products, "Еда/напитки", "Введите номер еды/напитка: ")
-    order["flavor"] = menu(flavors, "Вкусы", "Введите номер вкуса: ")
-    order["topping"] = menu(toppings, "Добавки и топпинги", "Введите номер топпинга: ")
+    order["product"] = menu(products, "🐱Еда/напитки🐱", "🐱Введите номер еды/напитка: ")
+    order["flavor"] = menu(flavors, "🐱Вкусы🐱", "🐱Введите номер вкуса: ")
+    order["topping"] = menu(toppings, "🐱Добавки и топпинги🐱", "🐱Введите номер топпинга: ")
     return order
 
 
 def print_order(order):
-    print("Имя: ", order["name"])
-    print("Еда/напиток: ", order["product"])
-    print("Вкус: ", order["flavor"])
-    print("Топпинг: ", order["topping"])
+    print("🐱Имя: ", order["name"])
+    print("🐱Еда/напиток: ", order["product"])
+    print("🐱Вкус: ", order["flavor"])
+    print("🐱Топпинг: ", order["topping"])
     return
 
 
 def save_order(order):
-    print("Заказ сохраняется...")
+    print("🐱Заказ сохраняется...")
     return
 
 
